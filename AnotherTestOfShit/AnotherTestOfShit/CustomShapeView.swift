14811107a95a4a269a8a984ef68ef1e46bb332c0
TripleMonkey <triplemonkeystudio@gmail.com>
2024-09-08 14:49:06 -0400

    MOD: refactor custom shape to accept parameters for corner sizes. add ability to adjust top corners

--- AnotherTestOfShit/AnotherTestOfShit/CustomShapeView.swift
+++ AnotherTestOfShit/AnotherTestOfShit/CustomShapeView.swift
@@ -7,54 +7,13 @@
 
 import SwiftUI
 
-// Rectangle with inverse rounded corners
-struct InverseBottomCornersRectangle: Shape {
-    // Create the Shape using path
-    func path(in rect: CGRect) -> Path {
-        /* To avoid hard coded values,
-         Shape rect width divided by 10 for
-         easily computing corner values
-         in 1/10 increments of total width
-         */
-        let tenthOfWidth = rect.maxX/10
-
-        var path = Path()
-        // Set path start point at bottom leading
-        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        // Draw path from start point to top leading
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        // Draw path from top leading to top trailing
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        // Draw path from top trailing to bottom trailing
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        // Draw inverse arc from bottom trailing
-        path.addArc(center: CGPoint(x: 9*tenthOfWidth, y: rect.maxY),
-                    radius: tenthOfWidth,
-                    startAngle: Angle(degrees: 0),
-                    endAngle: Angle(degrees: 270), clockwise: true)
-        // draw line to connect inside arcs
-        path.addLine(to: CGPoint(x: tenthOfWidth, y: rect.maxY-tenthOfWidth))
-        // Draw arc from bottom trailing corner top to corner bottom
-        path.addArc(center: CGPoint(x: (tenthOfWidth), y: rect.maxY),
-                    radius: tenthOfWidth,
-                    startAngle: Angle(degrees:270),
-                    endAngle: Angle(degrees: 180), clockwise: true)
-        // Connect the path to itself to keep smooth corner
-        path.closeSubpath()
-        // Return custom shape view
-        return path
-    }
-}
-
 struct CustomShapeView: View {
     // Place custom shape inside Geometry reader for framing
     var body: some View {
-        // Create shape as let for reuse
-        let inverseRounds = InverseBottomCornersRectangle()
+        // Initialize shape
+        let inverseRounds = InverseCornersRect()
             .fill(.clear)
-            .stroke(.blue, style: StrokeStyle(lineWidth: 10,
-                                              lineCap: .butt,
-                                              lineJoin: .round))
+            .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .butt, lineJoin: .round))
 
         GeometryReader() { proxy in
             // Check device screen ratio
@@ -75,3 +34,80 @@ struct CustomShapeView: View {
     CustomShapeView()
 }
 
+
+
+// Custom View- rectangle with inverse rounded corners
+struct InverseCornersRect: Shape {
+
+    // Corner parameters
+    var topRidgeWidth: CGFloat
+    var topRadius: CGFloat
+    var bottomRidgeWidth: CGFloat
+    var bottomRadius: CGFloat
+
+    init(topRidgeWidth: CGFloat = 0, topRadius: CGFloat = 0, bottomRidgeWidth: CGFloat = 0, bottomRadius: CGFloat = 1) {
+        // Keep all parameters from extending beyond 1/2 rect width
+        self.topRidgeWidth = topRidgeWidth<=4 ? topRidgeWidth : 4
+        self.topRadius = topRadius<=5 ? topRadius : 5
+        self.bottomRidgeWidth = bottomRidgeWidth<=4 ? bottomRidgeWidth : 4
+        self.bottomRadius = bottomRadius<=5 ? bottomRadius : 5
+    }
+
+    // Create the Shape using path
+    func path(in rect: CGRect) -> Path {
+
+        // Divide max width to calculate values
+        let tenthOfWidth = rect.maxX/10
+
+        // Top values
+        let tlRidge = (1+topRidgeWidth)*tenthOfWidth
+        let ttRidge = (9-topRidgeWidth)*tenthOfWidth
+        let tlRadius = topRadius*tenthOfWidth
+        let ttRadius = topRadius*tenthOfWidth
+
+        // Bottom values
+        let btRidge = (9-bottomRidgeWidth)*tenthOfWidth
+        let blRidge = (1+bottomRidgeWidth)*tenthOfWidth
+        let btRadius = bottomRadius*tenthOfWidth
+        let blRadius = bottomRadius*tenthOfWidth
+
+        // Create path
+        var path = Path()
+        // Set path start point at bottom leading
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        // Draw path from start point to top leading
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        // Top Lead corner
+        path.addArc(center: CGPoint(x: tlRidge, y: rect.minY),
+                    radius: tlRadius,
+                    startAngle: Angle(degrees: 180),
+                    endAngle: Angle(degrees: 90), clockwise: true)
+        // Top connecting line
+        path.addLine(to: CGPoint(x: ttRidge, y: rect.minY+tlRadius))
+        // Top trailing corner
+        path.addArc(center: CGPoint(x: ttRidge, y: rect.minY),
+                    radius: ttRadius,
+                    startAngle: Angle(degrees:90),
+                    endAngle: Angle(degrees: 0), clockwise: true)
+        // Top trailing ridge
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        // Path from top trailing to bottom trailing
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        // Draw inverse arc from bottom trailing
+        path.addArc(center: CGPoint(x: btRidge, y: rect.maxY),
+                    radius: btRadius,
+                    startAngle: Angle(degrees: 0),
+                    endAngle: Angle(degrees: 270), clockwise: true)
+        // Draw line to connect inside arcs
+        path.addLine(to: CGPoint(x: blRidge, y: rect.maxY-blRadius))
+        // Draw arc from bottom trailing corner top to corner bottom
+        path.addArc(center: CGPoint(x: blRidge, y: rect.maxY),
+                    radius: blRadius,
+                    startAngle: Angle(degrees:270),
+                    endAngle: Angle(degrees: 180), clockwise: true)
+        // Connect the path to itself to keep smooth corner
+        path.closeSubpath()
+        // Return custom shape view
+        return path
+    }
+}
